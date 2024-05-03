5d7c6fe54b69f14fc3fac191e323ab10a94f4813
Hocwin <146694692+Hocwin@users.noreply.github.com>
2024-05-03 09:21:38 +0700

    Note Dialog

--- lib/screens/note_list_services.dart
+++ lib/screens/note_list_services.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:notes_firestore/services/notes_service.dart';
+import 'package:notes_firestore/widgets/note_dialog.dart';
 
 class NoteListScreen extends StatefulWidget {
   const NoteListScreen({super.key});
@@ -24,41 +25,7 @@ class _NoteListScreenState extends State<NoteListScreen> {
           showDialog(
               context: context,
               builder: (context) {
-                return AlertDialog(
-                  content: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const Text('Add'),
-                      const Padding(
-                        padding: EdgeInsets.only(top: 8),
-                        child: Text('Title', textAlign: TextAlign.start),
-                      ),
-                      TextField(controller: _titleController),
-                      const Padding(
-                        padding: EdgeInsets.only(top: 20),
-                        child: Text('Description', textAlign: TextAlign.start),
-                      ),
-                      TextField(controller: _descriptionController)
-                    ],
-                  ),
-                  actions: [
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 10),
-                      child: ElevatedButton(
-                          onPressed: () {
-                            Navigator.of(context).pop();
-                          },
-                          child: Text('Cancel')),
-                    ),
-                    ElevatedButton(
-                        onPressed: () {
-                          NoteService.addNote(_titleController.text,
-                                  _descriptionController.text)
-                              .whenComplete(() => Navigator.of(context).pop());
-                        },
-                        child: Text('Save'))
-                  ],
-                );
+                return NoteDialog();
               });
         },
         tooltip: 'Add Note',
@@ -98,54 +65,7 @@ class NoteList extends StatelessWidget {
                         showDialog(
                             context: context,
                             builder: (context) {
-                              TextEditingController titleController =
-                                  TextEditingController(
-                                      text: document['title']);
-                              TextEditingController descriptionController =
-                                  TextEditingController(
-                                      text: document['description']);
-                              return AlertDialog(
-                                title: const Text('Update Notes'),
-                                content: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    const Text('Title',
-                                        textAlign: TextAlign.start),
-                                    TextField(
-                                      controller: titleController,
-                                    ),
-                                    const Padding(
-                                      padding: EdgeInsets.only(top: 20),
-                                      child: Text('Description',
-                                          textAlign: TextAlign.start),
-                                    ),
-                                    TextField(
-                                      controller: descriptionController,
-                                    )
-                                  ],
-                                ),
-                                actions: [
-                                  Padding(
-                                    padding: const EdgeInsets.symmetric(
-                                        horizontal: 10),
-                                    child: ElevatedButton(
-                                        onPressed: () {
-                                          Navigator.of(context).pop();
-                                        },
-                                        child: Text('Cancel')),
-                                  ),
-                                  ElevatedButton(
-                                      onPressed: () {
-                                        NoteService.updateNote(
-                                                document['id'],
-                                                titleController.text,
-                                                descriptionController.text)
-                                            .whenComplete(() =>
-                                                Navigator.of(context).pop());
-                                      },
-                                      child: Text('Update'))
-                                ],
-                              );
+                              return NoteDialog(note : document);
                             });
                       },
                       title: Text(document['title']),
